f5dbea27313525f50eb1e40b105cb2f44aa5917a
minced1 <66805829+minced1@users.noreply.github.com>
2024-06-10 09:14:09 +0000

    Initial commit

--- index.php
+++ index.php
@@ -1,27 +1,47 @@
 <?php
-
-/*----------------------------------------------------------------------------------------
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License. See LICENSE in the project root for license information.
- *---------------------------------------------------------------------------------------*/
-
-function sayHello($name) {
-	echo "Hello $name!";
-}
-
+/* 
+	if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
+		$uri = 'https://';
+	} else {
+		$uri = 'http://';
+	}
+	$uri .= $_SERVER['HTTP_HOST'];
+	header('Location: '.$uri.'/dashboard/');
+	exit; */
 ?>
-
+<!DOCTYPE html>
+<head>
+<title>Comments</title>
+<link rel="stylesheet" href="styles.css">
+<script src="script.js"></script> 
+</head>
 <html>
-	<head>
-		<title>Visual Studio Code Remote :: PHP</title>
-	</head>
-	<body>
-		<?php 
-		
-		sayHello('remote world');
-			
-		phpinfo(); 
-			
-		?>
-	</body>
+<body>
+<!--
+<div class = "login">
+<div>Sign Up</div>
+<form>
+<label for="email">Email</label><br/>
+<input type="email" id="email" name="email"><br/>
+<label for="password">Password</label><br/>
+<input type="password" id="password" name="password"><br/>
+<input type="submit" value="Submit">
+</form>
+</div>
+-->
+
+<div class="flex-container">
+	<div>	
+		<textarea type="text" name="comment" placeholder="Write your comment here..."></textarea>
+	</div>
+</div>
+<div class="flex-container">
+	<div>
+	<input type="text" name="username" placeholder="Username">
+	</div>
+	<div>
+	<input type="submit" value="Submit">
+	</div>
+</div>
+</body>
 </html>
